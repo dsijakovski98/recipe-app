f2bcf70362a0790b30dda43c8deb6537a823f7ef
dsijakovski98 <dsijakovski98@github.com>
2020-08-26 18:32:04 +0200

    Updates

---
+++ precache-manifest.d478f917de26deb5090f9a21b93b80c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8039939d5dde5551d4ff685819c0f88",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "017c8b675be3b3a81f92",
+    "url": "/recipe-app/static/css/main.f5282eb2.chunk.css"
+  },
+  {
+    "revision": "3d86dfafec9892b940df",
+    "url": "/recipe-app/static/js/2.894248d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.894248d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017c8b675be3b3a81f92",
+    "url": "/recipe-app/static/js/main.3493cec5.chunk.js"
+  },
+  {
+    "revision": "fc6e5076bcefd61482c9",
+    "url": "/recipe-app/static/js/runtime-main.b4c1aa53.js"
+  }
+]);
